7dd91270a48099f01aa9ac26e1f03e93f6228e22
Constant Druon <druon.constant@gmail.com>
2022-10-10 23:39:09 +0200

    refactor: 💡 remove unused import

--- components/layout/footer/index.tsx
+++ components/layout/footer/index.tsx
@@ -1,4 +1,3 @@
-import { Logo } from "components/logo";
 import Link from "next/link";
 import { getCurrentYear } from "utils/date";
 import { footerExternalLinks, footerInternalLinks, openCrispBox } from "utils/links";
