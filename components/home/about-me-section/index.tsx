83e99933fbcb35066723298505d2b3396ba417f2
Constant Druon <druon.constant@gmail.com>
2022-10-23 10:56:23 +0200

    fix: 🐛 fix some spelling mistakes

--- components/home/about-me-section/index.tsx
+++ components/home/about-me-section/index.tsx
@@ -6,14 +6,14 @@ export const AboutMeSection = () => (
   <Section>
     <div className="grid gap-16 grid-cols-1 md:grid-cols-2">
       <div className="space-y-8">
-        <h2 className="text-xl md:text-3xl leading-tight">A bit about me</h2>
+        <h2 className="text-xl md:text-3xl leading-tight">About me</h2>
         <p>
           Hi, my name is Constant. I am a {getAgeByBirthDate("03 Nov 1999")} years old Software Engineer student from
           France. I am passionate about building a better world through technology.
         </p>
         <p>
-          I have started two startups since I was 18 years old, and I am always looking for new ways to use technology
-          to make a positive impact on the world.
+          Since the age of 18, I have launched two startups and I am always looking for new ways to use technology to
+          make a positive impact on the world.
         </p>
       </div>
 
--- components/home/jobs-section/index.tsx
+++ components/home/jobs-section/index.tsx
@@ -14,8 +14,8 @@ export const JobsSection = ({ jobs }: JobsSectionProps) => (
       <div className="space-y-8">
         <h2 className="text-xl md:text-3xl leading-tight">My previous experiences</h2>
         <p>
-          I have gained valuable experience from my internships and my involvement with startups. These experiences have
-          helped me to grow as a person and to learn new skills.
+          I gained valuable experience through my internships and implication within startups. These experiences have
+          helped me to grow as a person and to acquire new skills.
         </p>
         <p>
           I have had the opportunity to work with passionate and motivated people, which has been a great experience.
